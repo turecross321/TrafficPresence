f6270be3bfcc9a43536ee9417a3c532769010783
Ture <51852312+turecross321@users.noreply.github.com>
2024-07-25 17:26:24 +0200

    Upload presence

--- bicycle.py
+++ bicycle.py
@@ -1,5 +1,12 @@
 import RPi.GPIO as GPIO
+from websocket import create_connection
 import time
+import json
+
+DISCORD_CLIENT_ID = "1258728548613619733"
+SERVER_PASSWORD = "password"
+SERVER_URL = "ws://127.0.0.1:8080"
+DISCORD_UPDATE_INTERVAL = 15 # can't be less than 15 seconds to work
 
 SPEED_SENSOR = 23 # physical pin 16
 CADENCE_SENSOR = 24 # physical pin 18
@@ -9,10 +16,14 @@ WHEEL_CIRCUMFERENCE = 1.885 # In meters
 SPEED_STOPPED_THRESHOLD = 3 # Amount of seconds before assuming that the bicycle has stopped moving
 CADENCE_STOPPED_THRESHOLD = 2
 
+CLIENT_TYPE_SENDER = 1
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(SPEED_SENSOR, GPIO.IN)
 GPIO.setup(CADENCE_SENSOR, GPIO.IN)
+
+start_time = time.time()
 
 speed = 0 # Speed of bicycle in meters per second
 last_speed_value = speed
@@ -20,7 +31,17 @@ last_speed_time = time.time()
 
 cadence = 0 # How fast the cyclist is pedaling in RPM
 last_cadence_value = cadence
-last_cadence_time = time.time()
+last_cadence_time = start_time
+
+last_discord_update_time = 0
+
+ws = create_connection(SERVER_URL, header={
+                                  "client-type": str(CLIENT_TYPE_SENDER),
+                                  "password": SERVER_PASSWORD,
+                                  "discord-client-id": DISCORD_CLIENT_ID,
+                                  "activity-name": "bicycle"
+                              })
+print("Connected to Presence Server")
 
 try:
     while True:
@@ -55,8 +76,24 @@ try:
 
         last_speed_value = speed_value
         last_cadence_value = cadence_value
+
+        if now - last_discord_update_time >= DISCORD_UPDATE_INTERVAL:
+                message = {
+                "state": "Cycling",
+                "details": f"{int(speed)} km/h | {int(cadence)} RPM",
+                "timestamps": {"start": start_time},
+                "assets": {
+                        "large_image": "bicycle", 
+                        "large_text": "Kayoba Elegance 28\""
+                        }
+                }
+                serialized = json.dumps(message)
+                ws.send(serialized)
+                print("Updated presence")
+                last_discord_update_time = now
+
         time.sleep(POLLING_DELAY)
 
 except KeyboardInterrupt:
-    # Cleanup GPIO settings before exiting
     GPIO.cleanup()
+    ws.close()
